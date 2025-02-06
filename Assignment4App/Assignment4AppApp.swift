c583e33c98a5963d64da80ac8b7567ec4f6cb776
Louise Rennick <wddtechnologies@trios.com>
2025-02-06 18:08:59 -0500

    Initial Commit

---
+++ Assignment4App/Assignment4AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment4AppApp.swift
+//  Assignment4App
+//
+//  Created by Louise Rennick on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct Assignment4AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
